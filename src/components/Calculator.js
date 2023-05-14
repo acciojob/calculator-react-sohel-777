4cf62932c5792952f0a21390803a112bc8eff2d7
sk sohel rahaman <sohelrahaman43@gmail.com>
2023-05-14 12:48:12 +0530

    submitting code-Sun May 14 2023 12:48:10 GMT+0530 (India Standard Time)

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -22,7 +22,7 @@ const Calculator = () => {
   };
 
   return (
-    <div className="calculator">
+    <div className="Calculator">
       <input
         className="input-field"
         type="text"
@@ -31,23 +31,23 @@ const Calculator = () => {
       />
 
       <div className="button-row">
-        <button className="gray-button" onClick={() => handleButtonPress("7")}>7</button>
-        <button className="gray-button" onClick={() => handleButtonPress("8")}>8</button>
-        <button className="gray-button" onClick={() => handleButtonPress("9")}>9</button>
+        <button id="btn-7" className="gray-button" onClick={() => handleButtonPress("7")}>7</button>
+        <button id="btn-8" className="gray-button" onClick={() => handleButtonPress("8")}>8</button>
+        <button id="btn-9" className="gray-button" onClick={() => handleButtonPress("9")}>9</button>
         <button className="gray-button" onClick={() => handleButtonPress("/")}>/</button>
       </div>
 
       <div className="button-row">
-        <button className="gray-button" onClick={() => handleButtonPress("4")}>4</button>
-        <button className="gray-button" onClick={() => handleButtonPress("5")}>5</button>
-        <button className="gray-button" onClick={() => handleButtonPress("6")}>6</button>
+        <button id="btn-4" className="gray-button" onClick={() => handleButtonPress("4")}>4</button>
+        <button id="btn-5" className="gray-button" onClick={() => handleButtonPress("5")}>5</button>
+        <button id="btn-6" className="gray-button" onClick={() => handleButtonPress("6")}>6</button>
         <button className="gray-button" onClick={() => handleButtonPress("*")}>*</button>
       </div>
 
       <div className="button-row">
-        <button className="gray-button" onClick={() => handleButtonPress("1")}>1</button>
-        <button className="gray-button" onClick={() => handleButtonPress("2")}>2</button>
-        <button className="gray-button" onClick={() => handleButtonPress("3")}>3</button>
+        <button id="btn-1" className="gray-button" onClick={() => handleButtonPress("1")}>1</button>
+        <button id="btn-2" className="gray-button" onClick={() => handleButtonPress("2")}>2</button>
+        <button id="btn-3" className="gray-button" onClick={() => handleButtonPress("3")}>3</button>
         <button className="gray-button" onClick={() => handleButtonPress("-")}>-</button>
       </div>
 
